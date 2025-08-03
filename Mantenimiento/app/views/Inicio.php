b50a744abd48a5ca187005305c9059b30bbcc6c1
JayC-17 <doblec17@outlook.com>
2025-08-03 10:10:44 -0600

    Cuarto cambio
    pequeños cambios

---
+++ Mantenimiento/app/views/Inicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <aside class="sidebar" style="width: 240px; background-color: #3771C8; color: #fff; padding: 20px; display: flex; flex-direction: column; position: fixed; height: 100%; top: 0; left: 0; z-index: 1000;">
+    <nav>
+      <ul>
+        <button>Cerar sesión</button>
+        <button>Configuración</button>
+      </ul>
+    </nav>
+  </aside>
+
+  <div class="main-container">
+    <header class="header">
+      <h1>Perfil</h1>
+    </header>
+  </div>
+</body>
+</html>
